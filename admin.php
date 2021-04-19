4778d58e624e1be8b32874e888736735aab66d89
BDelapo <b.delaportas@gmail.com>
2021-04-19 08:39:49 -0400

    added to index for admin and made changes to home

---
+++ admin.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+require("connectDB.php");
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Profile Page</title>
+		<link href="css/home.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>COVID-19 Dashboard</h1>
+                <a href="home.php"><i class="fas fa-home"></i>Home</a>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>User</h2>
+            <form 
+
+			<div>
+
+			
+			</div>
+		</div>
+	</body>
+</html>
