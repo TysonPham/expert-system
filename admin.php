1833ca29267b8510389c3edb99342e4a5ac1fcb3
BDelapo <b.delaportas@gmail.com>
2021-04-19 09:23:00 -0400

    added to admin

--- admin.php
+++ admin.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
 require("connectDB.php");
+
+if(isset($_POST['submitGetUser'])){
+    $medNum = $_POST['displayUser'];
+    $sql = "SELECT * FROM Person where med_Num = '$medNum'";
+    $result = mysqli_query($conn, $sql) or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error($conn), E_USER_ERROR);
+    $field = mysqli_fetch_row($result);
+    
+}
+//Add user not done
+else if(isset($_POST['submitAddUser'])){
+
+}
+
 ?>
 
 
@@ -22,8 +35,34 @@ require("connectDB.php");
 			</div>
 		</nav>
 		<div class="content">
-			<h2>User</h2>
-            <form 
+			<h2>Get User</h2>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"> 
+            <input type="text" name="displayUser" id="displayUser">
+            <input type="submit" name="submitGetUser">
+            </form>
+
+            <h2>Add User</h2>
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"> 
+            <input type="text" name="addUserMednum" >
+            <input type="text" name="addUserfirstname">
+            <input type="text" name="addUserlastname" >
+            <input type="text" name="addUserdob" >
+            <input type="text" name="addUsertelephone">
+            <input type="text" name="addUseremail">
+            <input type="text" name="addUserprovince">
+            <input type="text" name="addUseraddress">
+            <input type="text" name="addUserfirst3chars">
+            <input type="text" name="addUserlast3chars">
+            <input type="text" name="addUsercitizen">
+            <input type="text" name="addUserfirstparentmed">
+            <input type="text" name="addUsersecondparentmed">
+            <input type="submit" name="submitAddUser">
+
+            </form>
+
+           <?php 
+           for($x = 0; $x < 12; $x++ )
+           echo($field[$x] . " "); ?>
 
 			<div>
 
